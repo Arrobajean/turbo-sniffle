9be3f1574470450f55f93aac394a9ec8f4d14cfc
arrobajean <jean.castanedah@gmail.com>
2024-11-07 11:41:04 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
@@ -25,24 +26,17 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"], // Usa MiniCssExtractPlugin
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"], // Usa MiniCssExtractPlugin
       },
       {
-        test: /\.(png|jpeg|jpg|gif)$/i,
+        test: /\.(png|jpeg|jpg|gif|svg)$/i,
         type: "asset/resource",
         generator: {
-          filename: "images/[name][hash][ext]",
-        },
-      },
-      {
-        test: /\.svg$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "images/[name][hash][ext]",
+          filename: "images/[name][hash][ext]", // Mejora la gestión de imágenes
         },
       },
     ],
@@ -52,26 +46,30 @@ module.exports = {
       template: "./src/index.html",
       publicPath: "/ananda-portfolio-v3/",
     }),
-    new FaviconsWebpackPlugin({
-      logo: "./src/assets/ui/favicon.ico",
-      cache: true,
-      inject: true,
-      favicons: {
-        appName: "Ananda de Sousa - Portfolio",
-        appDescription: "Portfolio de Ananda de Sousa",
-        developerName: "Ananda de Sousa",
-        developerURL: null,
-        background: "#fff",
-        theme_color: "#333",
-        icons: {
-          coast: false,
-          yandex: false,
-        },
-      },
-    }),
+    // new FaviconsWebpackPlugin({
+    //   logo: "./src/assets/ui/favicon.ico",
+    //   cache: true,
+    //   inject: true,
+    //   favicons: {
+    //     appName: "Ananda de Sousa - Portfolio",
+    //     appDescription: "Portfolio de Ananda de Sousa",
+    //     developerName: "Ananda de Sousa",
+    //     developerURL: null,
+    //     background: "#fff",
+    //     theme_color: "#333",
+    //     icons: {
+    //       coast: false,
+    //       yandex: false,
+    //     },
+    //   },
+    // }),
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css", // Archivos CSS separados
+      chunkFilename: "[id].css",
     }),
   ],
   resolve: {
@@ -85,8 +83,9 @@ module.exports = {
       directory: path.join(__dirname, "dist"),
     },
     compress: true,
-    port: 8080,
+    port: 8081,
     historyApiFallback: true,
   },
-  mode: "development",
+  mode: "development", // Cambia a 'production' para la versión final
+  devtool: "source-map", // Habilita el mapa de fuentes para facilitar la depuración
 };
